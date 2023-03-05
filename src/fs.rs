03fb7b89db177300bd3c59b30ae5edb9c8deddb3
maxgamerboy1 <phillipstom60@gmail.com>
2023-03-05 01:02:17 +0000

    Add mut

--- src/fs.rs
+++ src/fs.rs
@@ -518,7 +518,7 @@ impl<Storage: driver::Storage> Filesystem<'_, Storage> {
         // let block_size = unsafe { c.read().block_size };
         let block_size = Storage::BLOCK_SIZE as u32;
         let off = (block * block_size + off) as usize;
-        let buf: &[u8] = unsafe { slice::from_raw_parts(buffer as *const u8, size as usize) };
+        let mut buf: &[u8] = unsafe { slice::from_raw_parts(buffer as *const u8, size as usize) };
 
         io::error_code_from(storage.write(off, &mut buf))
     }
